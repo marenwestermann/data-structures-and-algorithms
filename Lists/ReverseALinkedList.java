b08d9c6103d04aa52a053124b508da4dc0f7faaf
Maren Westermann <maren.westermann@gmail.com>
2020-03-19 14:04:38 +0100

    reverse a linked list

---
+++ Lists/ReverseALinkedList.java
@@ -0,0 +1,125 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class ReverseALinkedList {
+
+	static class SinglyLinkedListNode {
+		public int data;
+		public SinglyLinkedListNode next;
+
+		public SinglyLinkedListNode(int nodeData) {
+			this.data = nodeData;
+			this.next = null;
+		}
+	}
+
+	static class SinglyLinkedList {
+		public SinglyLinkedListNode head;
+		public SinglyLinkedListNode tail;
+
+		public SinglyLinkedList() {
+			this.head = null;
+			this.tail = null;
+		}
+
+		public void insertNode(int nodeData) {
+			SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+			if (this.head == null) {
+				this.head = node;
+			} else {
+				this.tail.next = node;
+			}
+
+			this.tail = node;
+		}
+	}
+
+	public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+		while (node != null) {
+			bufferedWriter.write(String.valueOf(node.data));
+
+			node = node.next;
+
+			if (node != null) {
+				bufferedWriter.write(sep);
+			}
+		}
+	}
+
+	// Complete the reverse function below.
+
+	/*
+	 * For your reference:
+	 *
+	 * SinglyLinkedListNode {
+	 *     int data;
+	 *     SinglyLinkedListNode next;
+	 * }
+	 *
+	 */
+	static SinglyLinkedListNode reverse(SinglyLinkedListNode head) {
+		
+		// special case: the list is empty
+		if (head == null) {
+			return head;
+		}
+
+		// keep track of current node (pointer)
+		SinglyLinkedListNode current = head;
+		while (current.next != null) {
+			// store next node in variable
+			SinglyLinkedListNode node = current.next;
+			// store node after next node in variable
+			// (becomes head of remaining linked list)
+			SinglyLinkedListNode node2 = current.next.next;
+			// prepend node to head (change link)
+			node.next = head;
+			// update head pointer
+			head = node;
+			// create link to remaining linked list
+			current.next = node2;
+		} 
+
+		return head;
+
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+		int tests = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		for (int testsItr = 0; testsItr < tests; testsItr++) {
+			SinglyLinkedList llist = new SinglyLinkedList();
+
+			int llistCount = scanner.nextInt();
+			scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+			for (int i = 0; i < llistCount; i++) {
+				int llistItem = scanner.nextInt();
+				scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+				llist.insertNode(llistItem);
+			}
+
+			SinglyLinkedListNode llist1 = reverse(llist.head);
+
+			printSinglyLinkedList(llist1, " ", bufferedWriter);
+			bufferedWriter.newLine();
+		}
+
+		bufferedWriter.close();
+
+		scanner.close();
+	}
+}
+

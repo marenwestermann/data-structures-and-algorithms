58929cd7462ef7676db223355be1f482e71e4d30
Maren Westermann <maren.westermann@gmail.com>
2020-03-19 14:10:07 +0100

    add problem statement

--- Lists/ReverseALinkedList.java
+++ Lists/ReverseALinkedList.java
@@ -1,3 +1,8 @@
+/* Problem: 
+You’re given the pointer to the head node of a linked list. Change the next pointers 
+of the nodes so that their order is reversed. The head pointer given may be null 
+meaning that the initial list is empty. */
+
 import java.io.*;
 import java.math.*;
 import java.security.*;

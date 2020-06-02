9debee8ac3bc0a97e88ad1fdfb8a2d52a666a962
Maren Westermann <maren.westermann@gmail.com>
2020-06-02 12:01:41 +0200

    add documentation and change indentation from tabs to spaces

--- Recursion/RecursionFibonacciNumbers.java
+++ Recursion/RecursionFibonacciNumbers.java
@@ -1,25 +1,35 @@
+/* Problem:
+Calculate the nth Fibonacci number.
+
+Constraints:
+0 < n ≤ 30
+
+Note:
+this is my recursive implementation. An iterative
+implmentation can be found in the "Arrays" folder. */
+
 import java.util.*;
 
 public class RecursionFibonacciNumbers {
 
-	public static int fibonacci(int n) {
+    public static int fibonacci(int n) {
 
-		// create base cases
-		if (n == 0) {
-			return 0;
-		} else if (n == 1) {
-			return 1;
-		} else { 
-			return fibonacci(n-1) + fibonacci(n-2);
-		}
-	}
+        // create base cases
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
 
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
-		int n = scanner.nextInt();
-		scanner.close();
-		System.out.println(fibonacci(n));
-	}
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        System.out.println(fibonacci(n));
+    }
 }

0276c604781e904db4bd50b65349414bb727a0d4
Maren Westermann <maren.westermann@gmail.com>
2020-06-16 11:42:44 +0200

    add tree postorder traversal

---
+++ Trees/TreePostorderTraversal.java
@@ -0,0 +1,59 @@
+/* Problem: print the values of the nodes in a binary tree using post-order traversal.
+Note: there are no key:value pairs in the tree, only integer values. */
+
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class TreePostorderTraversal {
+
+    public static void postOrder(Node root) {
+
+        // base case 
+        if (root == null) return;
+
+        // recursion
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        postOrder(root);
+    }
+}
